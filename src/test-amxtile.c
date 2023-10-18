e9caf884e7a0f615f73c3b4e0cdb0290fa14052f
pascalrb <pbastie@artemisia.ece.ncsu.edu>
2023-10-18 18:17:52 -0400

    On volatile server. Saving code
    - Tinkering with _tile_load() and _tile_dpbssd()
    - Timing comparison of matmul using amx tiles vs conventional cpu ex units

--- src/test-amxtile.c
+++ src/test-amxtile.c
@@ -10,6 +10,7 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define MAX 1024
 #define MAX_ROWS 16
@@ -26,8 +27,14 @@ typedef struct __tile_config
   uint8_t palette_id;
   uint8_t start_row;
   uint8_t reserved_0[14];
-  uint16_t colsb[16]; 
-  uint8_t rows[16]; 
+  //uint16_t colsb[16]; 
+  
+  uint16_t colsb[8]; 
+  uint8_t reserved_1[16]; 
+
+  //uint8_t rows[16]; 
+  uint8_t rows[8]; 
+  uint8_t reserved_2[8]; 
 } __tilecfg;
 
 /* Initialize tile config */
@@ -37,14 +44,17 @@ static void init_tile_config (__tilecfg *tileinfo)
   tileinfo->palette_id = 1;
   tileinfo->start_row = 0;
 
-  for (i = 0; i < 1; ++i)
-  {
-    tileinfo->colsb[i] = MAX_ROWS;
-    tileinfo->rows[i] =  MAX_ROWS;
-  }
+  //for (i = 0; i < 1; ++i)
+  //{
+  //  // 1 Tile - 16x16
+  //  //tileinfo->colsb[i] = MAX_ROWS;
+  //  //tileinfo->rows[i] =  MAX_ROWS;
+  //}
+  // Not used
 
   for (i = 1; i < 4; ++i)
   {
+    // 3 Tiles - each 16x64
     tileinfo->colsb[i] = MAX_COLS;
     tileinfo->rows[i] =  MAX_ROWS;
   }
@@ -126,43 +136,109 @@ static void print_buffer32(int32_t* buf, int32_t rows, int32_t colsb)
 
 int main(){
 
-   __tilecfg tile_data = {0};
-   int8_t src1[MAX];
-   int8_t src2[MAX];
-   int32_t res[MAX/4];
-   int rows  = MAX_ROWS;
-   int colsb = MAX_COLS;
+  __tilecfg tile_data = {0};
+  int8_t src1[MAX];
+  int8_t src2[MAX];
+  int32_t res[MAX/4];
+  int rows  = MAX_ROWS;
+  int colsb = MAX_COLS;
 
-   // Request permission to linux kernel to run AMX 
-   if (!set_tiledata_use())
-      exit(-1);
+  clock_t start0 = clock();
+  // Request permission to linux kernel to run AMX 
+  if (!set_tiledata_use())
+     exit(-1);
 
-   // Load tile configuration 
-   init_tile_config (&tile_data);
+  // Load tile configuration 
+  init_tile_config (&tile_data);
 
-   // Init src matrix buffers with data
-   init_buffer (src1, 2);
-   print_buffer(src1, rows, colsb);
+  // Init src matrix buffers with data
+  init_buffer (src1, 2);
+  //print_buffer(src1, rows, colsb);
  
-   init_buffer (src2, 2);
-   print_buffer(src2, rows, colsb);
+  init_buffer (src2, 4);
+  //print_buffer(src2, rows, colsb);
 
-   // Init dst matrix buffers with data
-   init_buffer32 (res, 0);
+  // Init dst matrix buffers with data
+  init_buffer32 (res, 0);
 
-   // Load tile rows from memory
-   _tile_loadd (2, src1, STRIDE);
-   _tile_loadd (3, src2, STRIDE);
-   _tile_loadd (1, res, STRIDE);
+  // Load tile rows from memory into tile registers
+  //_tile_loadd (2, src1, STRIDE);    // Load src1 into Tile2 reg, from *src1, STRIDE bytes at a time
+  //_tile_loadd (3, src2, STRIDE);
+  //_tile_loadd (1, res, STRIDE);
+  printf("here0\n");
+  _tile_loadd(1, src1, STRIDE);    // Load src1 into Tile2 reg, from *src1, STRIDE bytes at a time
+  printf("here1\n");
+  _tile_loadd(2, src2, STRIDE);
+  printf("here2\n");
+  _tile_loadd(3, res, STRIDE);
+  printf("here3\n");
 
-   // Compute dot-product of bytes in tiles 
-   _tile_dpbssd (1, 2, 3);
+  //clock_t start0 = clock();
+  // Compute dot-product of bytes in tiles 
+  //_tile_dpbssd (1, 2, 3);
+  _tile_dpbssd (3, 2, 1);
+  printf("here4\n");
+  //clock_t end0 = clock();
 
-   // Store the tile data to memory
-   _tile_stored (1, res, STRIDE);
-   print_buffer32(res, rows, colsb/4);
+  // Store the tile data to memory
+  //_tile_stored (1, res, STRIDE);
+  _tile_stored (3, res, STRIDE);
+  printf("here5\n\n");
+  clock_t end0 = clock();
 
-   // Release the tile configuration to return to the init state, 
-   // which releases all storage it currently holds
-   _tile_release ();
+  double mat_mul_amx = (double)(end0-start0)/ CLOCKS_PER_SEC;
+
+  printf("Result AMX:\n");
+  print_buffer32(res, rows, colsb/4);
+
+  //Conventional Mat Mul 
+  int8_t A[rows][colsb];
+  int8_t B[colsb][rows];
+  int32_t C[rows][rows];
+  clock_t start1 = clock();
+  for(int i = 0; i < rows; i++){
+   for(int j = 0; j < colsb; j++){
+     A[i][j] = 2;
+   }
+  }
+  for(int i = 0; i < colsb; i++){
+   for(int j = 0; j < rows; j++){
+     B[i][j] = 4;
+   }
+  }
+  //clock_t start1 = clock();
+  //Matmul
+  for(int i = 0; i < rows; i++){
+   for(int j = 0; j < rows; j++){
+     C[i][j] = 0;
+     for(int k = 0; k < colsb; k++){
+       C[i][j] += A[i][k] * B[k][j];
+     }
+   }
+  }
+  clock_t end1 = clock();
+  double mat_mul_conv = (double)(end1-start1)/ CLOCKS_PER_SEC;
+
+  printf("Result Conventional:\n");
+  //Print C
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < rows; j++){
+      printf("%d ", C[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  
+  printf("Time for AMX:          %lf\n", mat_mul_amx);
+  printf("Time for conventional: %lf\n", mat_mul_conv);
+
+  //TODO: 
+  //1. understand tileconfig initalization
+  //2. Figure out how we can use TILE regs on large containers
+  //  (~1MB of data)
+
+
+  // Release the tile configuration to return to the init state, 
+  // which releases all storage it currently holds
+  _tile_release ();
 }
